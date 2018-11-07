95cbd3812843e8955bd5e46a57a781be7f608604
Nati <33567785+DevilN31@users.noreply.github.com>
2018-11-07 20:59:25 +0200

    BulletHoleProject

---
+++ Assets/GunControll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunControll : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletHole;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, bulletHole.position, bulletHole.rotation);
+            bullet.GetComponent<Rigidbody>().AddForce(Vector3.forward * 1000);
+            Destroy(bullet, 2);
+        }
+	}
+}
