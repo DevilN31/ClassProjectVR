b0e3d0130eb813f225b3bde6b9ffd6eaba6f056d
Nati <33567785+DevilN31@users.noreply.github.com>
2018-11-18 20:21:47 +0200

    Change to Android VR game
    Changed project to Android
    Added Cube enemies

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+	void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "Player")
+        {
+            Debug.Log(this.name + " Killed");
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+
+        }
+    }
+}
